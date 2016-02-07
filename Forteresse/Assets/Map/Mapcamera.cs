dc9d068ad2dc7e59e1eaf81ab1ac79ae7a2fffb5
ridsar <riday.sarkar@epita.fr>
2016-02-07 18:12:00 +0100

    ajout : map, un perso, deux maisons avec la tour

---
+++ Forteresse/Assets/Map/Mapcamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mapcamera : MonoBehaviour {
+
+
+    public Transform Player;
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(Player.position.x, transform.position.y, Player.position.z);
+    }
+}
