1680299b860ef48e2930e1407d2a65dcce689e12
Slourf <florian.chatelus@yahoo.fr>
2016-02-11 15:43:09 +0100

    amélioration du saut du personnage

--- Forteresse/Assets/Jump.cs
+++ Forteresse/Assets/Jump.cs
@@ -1,34 +1,25 @@
 ﻿using UnityEngine;
+using System.Collections;
 
-[RequireComponent(typeof(CharacterController))]
-
-public class FPSWalker : MonoBehaviour
+public class Jump : MonoBehaviour
 {
-    public float speed = 6.0f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
-
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
-    private bool grounded = false;
-
-    void FixedUpdate()
+    void Update()
     {
-        if (grounded)
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
         {
-            // We are grounded, so recalculate movedirection directly from axes
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
 
-            if (Input.GetButton("Jump")) moveDirection.y = jumpSpeed;
         }
-
-        // Apply gravity
         moveDirection.y -= gravity * Time.deltaTime;
-
-        // Move the controller
-        /*CharacterController controller = (CharacterController)GetComponent(typeof(CharacterController));
-        CollisionFlags flags = controller.Move(moveDirection * Time.deltaTime);
-        grounded = (flags  CollisionFlags.CollidedBelow) != 0;*/
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
