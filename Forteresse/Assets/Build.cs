4467f28c3e30f122c4e4d2ca3e4e4896cd260966
Slourf <florian.chatelus@yahoo.fr>
2016-02-13 03:18:40 +0100

    merge

--- Forteresse/Assets/Build.cs
+++ Forteresse/Assets/Build.cs
@@ -5,30 +5,66 @@ public class Build : MonoBehaviour
 {
     public GameObject[] Tower;
     private Vector3 spawnPoints;
+    bool canBuild = true; //FIXEME
 
     void Update()
     {
+
+
+        //Déclaration variables
         Tower = GameObject.FindGameObjectsWithTag("Tower");
         var Field = GameObject.Find("Terrain");
 
-        if (Input.GetMouseButton(1))
+
+
+        //Instanciation d'un model de tour pour visualisation
+       /* if (Input.GetKey(KeyCode.Alpha1) && !canBuild)
         {
             RaycastHit hit;
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                canBuild = true;
 
+                float x = hit.point.x;
+                float y = hit.point.y;
+                float z = hit.point.z;
+
+                StartCoroutine(spawnEnemy(x, y, z));
+                var tour = GameObject.Find("Tower(Clone)");                              
+            }         
+        }*/
+
+
+        //l'objet suit la souris
+        /*if (canBuild)
+        {
+            RaycastHit hit;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
             {
-                float x =  hit.point.x;
+                var tour = GameObject.Find("Tower(Clone)");
+                tour.transform.position = hit.point;
+            }
+        }*/
+            
+
+        //l'objet est construit pour de vrai en jeu 
+        if (Input.GetMouseButton(1) && canBuild)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                float x = hit.point.x;
                 float y = hit.point.y;
                 float z = hit.point.z;
                 if (y == Field.transform.position.y)
                     StartCoroutine(spawnEnemy(x, y, z));
+                    canBuild = true; // FIXEME
             }
-        }
-
-        
-
+        }       
     }
 
+
+    //Clone l'objet voulu
     IEnumerator spawnEnemy(float x, float y, float z)
     {
         spawnPoints.x = x;
