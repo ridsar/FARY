ceb3c34261b767c22be22d549e2e74057b4e5349
Slourf <florian.chatelus@yahoo.fr>
2016-02-14 02:45:02 +0100

    modification script BUILD: non concluante

--- Forteresse/Assets/Build.cs
+++ Forteresse/Assets/Build.cs
@@ -6,12 +6,13 @@ public class Build : MonoBehaviour
 {
     public GameObject[] Tower;
     private Vector3 spawnPoints;
-    bool canBuild = false; //FIXEME
+    bool canBuild = false;
 
 
     void Update()
     {
 
+        this.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
 
         //Déclaration variables
         Tower = GameObject.FindGameObjectsWithTag("Tower");
@@ -40,10 +41,17 @@ public class Build : MonoBehaviour
         //l'objet suit la souris
         if (canBuild)
         {
+
             var tour = GameObject.Find("Tower(Clone)");
+            var boule = GameObject.Find("Sphere");
+
+            tour.GetComponent<MeshRenderer>().material.mainTexture = tour.GetComponent<MeshRenderer>().material.GetTexture("Tower 1");
+            boule.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.2f);
+            tour.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.2f);
             tour.GetComponent<Collider>().enabled = false;
+            Cursor.visible = false;
+
             Vector3 mousePosition = new Vector3(Input.mousePosition.x, 100, Input.mousePosition.y / 2);
-            print(Input.mousePosition);
             Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
             objPosition.y = 0;
             tour.transform.position = objPosition;
@@ -52,18 +60,19 @@ public class Build : MonoBehaviour
        
 
         //l'objet est construit pour de vrai en jeu 
-        if (Input.GetMouseButton(1) && canBuild)
+        if (Input.GetMouseButton(0) && canBuild)
         {
             RaycastHit hit;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
             {
                 var tour = GameObject.Find("Tower(Clone)");
                 tour.GetComponent<Collider>().enabled = true;
+                Cursor.visible = true;
                 tour.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
                 float x = hit.point.x;
                 float y = hit.point.y;
                 float z = hit.point.z;
-                canBuild = false; // FIXEME
+                canBuild = false;
             }
             GameObject.Find("Tower(Clone)").name = "Tower(Build)";
         }       
