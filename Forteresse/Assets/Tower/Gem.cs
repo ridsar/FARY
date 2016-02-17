5a0d9a23d58c5a343941fa8030b3dc2e057e1c35
Slourf <florian.chatelus@yahoo.fr>
2016-02-17 22:21:24 +0100

    Modif script pour faire bouger le crystal -> provoque des erreurs ne pas faire attention git add .

--- Forteresse/Assets/Tower/Gem.cs
+++ Forteresse/Assets/Tower/Gem.cs
@@ -5,19 +5,23 @@ public class Gem : MonoBehaviour {
     public float speed = 0.5f;
     public float altitude_up = 1;
     public float altitude_down = 10;
+    Rigidbody rb;
 
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (transform.position.y >= altitude_up)
+        if (transform.position.y < 10)
+            rb.AddForce(0, 20, 0);
+        /*if (transform.position.y >= altitude_up)
             transform.position += new Vector3(0, -speed, 0) * Time.deltaTime;
         if (transform.position.y <= altitude_down)
-            transform.position += new Vector3(0, speed, 0) * Time.deltaTime;
+            transform.position += new Vector3(0, speed, 0) * Time.deltaTime;*/
     }
 }
