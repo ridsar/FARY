119613b5a38948dc81c275e8126f54427f877b2f
Slourf <florian.chatelus@yahoo.fr>
2016-02-19 16:00:48 +0100

    modif crystal

---
+++ Forteresse/Assets/Attack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RaycastHit hit;
+        Vector3 p1 = transform.position;
+        float distance = 0f;
+
+        if(Physics.SphereCast(p1, 10f, transform.forward, out hit, 10f))
+        {
+            distance = hit.distance;
+            print(distance);
+        }
+	}
+}
