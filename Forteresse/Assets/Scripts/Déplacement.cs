d7ab113edb36f31062b9f149915a65a4f3557c70
ridsar <riday.sarkar@epita.fr>
2016-02-20 23:40:18 +0100

    reseau - deplacement en reseau reste a faire

--- Forteresse/Assets/Scripts/Déplacement.cs
+++ Forteresse/Assets/Scripts/Déplacement.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class Déplacement : MonoBehaviour
+public class Déplacement : NetworkBehaviour
 {
 
     // Use this for initialization
