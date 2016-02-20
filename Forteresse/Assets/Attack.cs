a2c625cd648246ed82bb1b9b6bdbbb907df5a4a1
Slourf <florian.chatelus@yahoo.fr>
2016-02-20 13:42:14 +0100

    modif script pour gestion des degats: toujours pas au point

--- Forteresse/Assets/Attack.cs
+++ Forteresse/Assets/Attack.cs
@@ -4,36 +4,46 @@ using System.Collections;
 public class Attack : MonoBehaviour {
 
     public int damage = 5;
+    public GameObject[] Proj;
+    private Vector3 sommet;
 
     // Use this for initialization
     void Start ()
     {
 	
 	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        InvokeRepeating("attack", 0, 2);
-	}
 
+    // Update is called once per frame
+    void Update()
+    {
+        InvokeRepeating("attack", 0, 300);
+               
+    }
     void attack()
     {
+        Proj = GameObject.FindGameObjectsWithTag("Damage");
+
         RaycastHit hit;
         Vector3 p1 = transform.position;
-        float distance = 0f;
 
-        if (Physics.SphereCast(p1, 10f, transform.forward, out hit, 10f))
+        if (Physics.SphereCast(p1, 10f, transform.forward, out hit, 10f) && hit.transform.tag == "Enemy")
         {
-            damagePlayer _health = new damagePlayer(0);
-            distance = hit.distance;
-            if (hit.collider.tag == "Enemy")
-            {
-                int health = _health.getHealth();
-
-                health -= damage;
-            }
-
+            sommet = transform.position;
+            print(hit.collider);
+            Vector3 direction = hit.transform.position;
+            StartCoroutine(spawnProjectile(sommet, direction));
+            Destroy(GameObject.Find("Projectile(Clone)"));
+            
         }
+    }
+
+    IEnumerator spawnProjectile(Vector3 sommet, Vector3 direction)
+    {
+        sommet += new Vector3(0, 2, 0);
+        Instantiate(Proj[UnityEngine.Random.Range(0, 0)], sommet, Quaternion.identity).name = "Projectile(Clone)";
+        GameObject.Find("Projectile(Clone)").transform.position = Vector3.MoveTowards(transform.position, direction, 0);
+        CancelInvoke();
+        yield return new WaitForSeconds(0);
+
     }
 }
--- Forteresse/Assets/damagePlayer.cs
+++ Forteresse/Assets/damagePlayer.cs
@@ -6,6 +6,9 @@ public class damagePlayer : MonoBehaviour
     public int playerHealth = 30;
     int damage = 10;
 
+    public GameObject[] Proj;
+    Vector3 sommet;
+
     void Start()
     {
         
@@ -13,11 +16,11 @@ public class damagePlayer : MonoBehaviour
 
     void Update()
     {
-        
+        Proj = GameObject.FindGameObjectsWithTag("Damage");
     }
 
 
-    public damagePlayer (int health)
+    /*public damagePlayer (int health)
     {
         this.playerHealth = health;
     }
@@ -27,17 +30,28 @@ public class damagePlayer : MonoBehaviour
     public int getHealth()
     {
         return this.playerHealth;
-    }
+    }*/
 
 
 
     void OnCollisionEnter(Collision _collision)
     {
-        if(_collision.gameObject.tag == "Tower")
+        if(_collision.gameObject.tag == "Damage")
         {
+            var Proj = GameObject.Find("Projectile");
             playerHealth -= damage;
             print("enemyDong just touch..." + playerHealth);
         }
     }
 
+    IEnumerator spawnProjectile(float x, float y, float z)
+    {
+        sommet.x = x;
+        sommet.y = y;
+        sommet.z = z;
+
+        Instantiate(Proj[UnityEngine.Random.Range(0, Proj.Length - 1)], sommet, Quaternion.identity).name = "Projectile";
+        CancelInvoke();
+        yield return new WaitForSeconds(0);
+    }
 }
