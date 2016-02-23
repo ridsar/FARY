dae2e840a867395078549738b68765da775d9159
henric_a <arnaud.henric@epita.fr>
2016-02-23 16:30:26 +0100

    menu pause fix avec boutons resume, mainmenu, quit, load fonctionnels ! Le temps s'arrête également lorsqu'on est dans le menu pause et reprend quand on en sort !

---
+++ Forteresse/Assets/PauseScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+
+    GameObject PauseMenu;
+    bool paused;
+
+	void Start () {
+        paused = false;
+        PauseMenu = GameObject.Find("PauseMenu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+        }
+        if (paused)
+        {
+            PauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else if (!paused)
+        {
+            PauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+	
+	}
+
+    public void Resume()
+    {
+        paused = false;
+    }
+
+    public void MainMenu ()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Save ()
+    {
+        PlayerPrefs.SetInt("currentscenesave", 1);
+    }
+    public void Load()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetInt("currentscenesave"));
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
