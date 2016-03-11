79f7d742179a95adafa89d57d94b19e17c05c4ef
Slourf <florian.chatelus@yahoo.fr>
2016-03-11 18:54:56 +0100

    modif target tour

---
+++ Forteresse/Assets/Scripts/Attack/Target.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+    private Transform target = null;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (target == null)
+            return;
+        else
+            transform.LookAt(target);
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Enemy" && target == null)
+            target = other.transform;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            target = null;
+        }
+    }
+}
