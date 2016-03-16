db03d7f0c523fd0a56901be90f796001138751de
henric_a <arnaud.henric@epita.fr>
2016-03-16 17:06:29 +0100

    animations sur héros fix

---
+++ Forteresse/Assets/Animations.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animations : MonoBehaviour {
+
+    public Animator anim;
+    private float inputH;
+    private float inputV;
+    private bool jump;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        anim.SetFloat("inputH", inputH);
+        anim.SetFloat("inputV", inputV);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetBool("jump", true);
+        }
+        else
+        {
+            anim.SetBool("jump", false);
+        }
+    }
+}
