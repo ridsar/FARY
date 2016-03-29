5ed47e610c6747e1246ffeb97810dcf68db123a4
henric_a <arnaud.henric@epita.fr>
2016-03-29 22:30:35 +0200

    menu selection hero

--- Forteresse/Assets/Scripts/Menu/menuScript.cs
+++ Forteresse/Assets/Scripts/Menu/menuScript.cs
@@ -12,14 +12,20 @@ public class menuScript : MonoBehaviour {
     public Button exitText;
     public Camera CameraComp;
     public Text Play;
+    public Button Play2;
+    public Text Play2Text;
     public Text Multi;
     public Text Settings;
     public Text Exit;
     public Canvas SettingsMenu;
+    public Canvas PlayMenu;
     public Button Goback;
+    public Button Goback2;
     public float distance;
     public float lift;
     public bool cameraTurnIsFixed = false;
+    public bool overlordpick = false;
+    public bool magepick = false;
 
         // Use this for initialization
     void Start ()
@@ -64,6 +70,20 @@ public class menuScript : MonoBehaviour {
         CameraComp.transform.position = new Vector3((float)-30, (float)-7, (float)207) + new Vector3(0, lift, distance);
     }
 
+    public void play ()
+    {
+        CameraComp.transform.position = new Vector3((float)-30, (float)-7, (float)207) + new Vector3(0, lift, distance);
+        startText.enabled = false;
+        multiplayerText.enabled = false;
+        settingsText.enabled = false;
+        exitText.enabled = false;
+        Play.enabled = false;
+        Multi.enabled = false;
+        Settings.enabled = false;
+        Exit.enabled = false;
+        PlayMenu.enabled = true;
+    }
+
     public void StartLevel()
     {
         SceneManager.LoadScene(1);
@@ -73,9 +93,22 @@ public class menuScript : MonoBehaviour {
     {
         SceneManager.LoadScene(2);
     }
+    public void rightpick()
+    {
+        overlordpick = true;
+        magepick = false;
+        Play2.enabled = true;
+    }
+    public void leftpick()
+    {
+        overlordpick = false;
+        magepick = true;
+        Play2.enabled = true;
+    }
     public void GoBack ()
     {
         SettingsMenu.enabled = false;
+        PlayMenu.enabled = false;
         startText.enabled = true;
         multiplayerText.enabled = true;
         settingsText.enabled = true;
---
+++ Forteresse/Assets/SelectHero.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectHero : MonoBehaviour {
+
+    public bool overlordpick;
+    public GameObject overlordobject;
+    public GameObject mageobject;
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (overlordpick)
+        {
+
+        }
+
+    }
+}
