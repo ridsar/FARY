481e9c3fcd9dd50150b2465d15dbcf11fc190c98
Slourf <florian.chatelus@yahoo.fr>
2016-04-02 00:34:06 +0200

    ajout script money mais pas utilisé

---
+++ Forteresse/Assets/Scripts/Other/Coin.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.tag != "Field" && other.transform.tag != "Player")
+            Physics.IgnoreCollision(gameObject.GetComponent<SphereCollider>(), other.collider);
+    }
+}
---
+++ Forteresse/Assets/Scripts/Other/pickUpMoney.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickUpMoney : MonoBehaviour {
+
+    int money = 0;
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.transform.tag == "Money")
+        {
+            Destroy(other.gameObject);
+            ++money;
+            print(money);
+        }
+    }
+}
