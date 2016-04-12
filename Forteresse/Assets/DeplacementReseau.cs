a8aaa61ed1543bdcaac609d75a5083a1a0983c9b
sarrid <ridaysarkar60@gmail.com>
2016-04-12 03:39:00 +0200

    Reseau : deux perso + camera sui les deux + mvt perso

---
+++ Forteresse/Assets/DeplacementReseau.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class DeplacementReseau : NetworkBehaviour {
+
+    // Use this for initialization
+    public Rigidbody rb;
+    public float Speed;
+    public float rotateSpeed;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            //var GObj = GameObject.Find("Terrain");
+            Vector3 rotate = new Vector3();
+            Vector3 move = new Vector3();
+
+            RaycastHit hit;
+            float theDistance;
+
+            Vector3 downSide = transform.TransformDirection(Vector3.down) * 10;
+
+            if (Physics.Raycast(transform.position, (downSide), out hit))
+                theDistance = hit.distance;
+
+            double cosAngle = Mathf.Cos(transform.eulerAngles.y * Mathf.PI / 180);
+            double sinAngle = Mathf.Sin(transform.eulerAngles.y * Mathf.PI / 180);
+            if (Input.GetKey(KeyCode.Z))
+            {
+                move.z = move.z + (Speed * (float)cosAngle);
+                move.x = move.x + (Speed * (float)sinAngle);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                move.z = move.z - (Speed * (float)cosAngle);
+                move.x = move.x - (Speed * (float)sinAngle);
+
+            }
+            if (Input.GetKey(KeyCode.Z) && Input.GetKey(KeyCode.LeftShift))
+            {
+                move.z = move.z + (0.3f * (float)cosAngle);
+                move.x = move.x + (0.3f * (float)sinAngle);
+            }
+
+            if (Input.GetKey(KeyCode.Q))
+            {
+                move.x -= Speed * (float)cosAngle;
+                move.z += Speed * (float)sinAngle;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                move.x += Speed * (float)cosAngle;
+                move.z -= Speed * (float)sinAngle;
+            }
+
+            theDistance = hit.distance;
+
+            if (Input.GetKey(KeyCode.Space) && theDistance < 0.5)
+                rb.AddForce(0, 80, 0);
+
+            if (theDistance > 2)
+                rb.AddForce(0, -50, 0);
+
+            transform.Rotate(rotate, 5);
+            transform.position += move;
+        }
+    }
+}
---
+++ Forteresse/Assets/LookMouseReseau.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class LookMouseReseau : NetworkBehaviour {
+
+    public float lookSensitivity = 5f;
+    public float xRotation;
+    public float yRotation;
+    public float currentXRotation;
+    public float currentYRotation;
+    public float xRotationV;
+    public float yRotationV;
+    public float lookSmoothDamp = 0.1f;
+
+    CursorLockMode wantedMode;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            if (Input.GetMouseButton(0))
+            {
+            }
+            else
+            {
+                yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
+                transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+            }
+        }
+    }
+}
