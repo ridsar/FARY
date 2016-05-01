e5a4d343685c1ce7fa6781a43134b4d426a21dbe
Slourf <florian.chatelus@yahoo.fr>
2016-05-01 16:19:02 +0200

    fix select menu

--- Forteresse/Assets/Scripts/Menu/SelectHero.cs
+++ Forteresse/Assets/Scripts/Menu/SelectHero.cs
@@ -11,11 +11,12 @@ public class SelectHero : MonoBehaviour {
 
     public GameObject light_mage;
     public GameObject light_warrior;
+    public Button readyB;
 
     GameObject PauseMenu;
-    GameObject overlord;
-    GameObject mage;
-    GameObject fire;
+    public GameObject overlord;
+    public GameObject mage;
+    public GameObject fire;
     public GameObject pause;
     bool paused;
     bool selectR = false;
@@ -33,20 +34,13 @@ public class SelectHero : MonoBehaviour {
         Cursor.visible = true;
         GameObject.Find("Main Camera").GetComponent<WowCamera>().enabled = false; //F
         GameObject.Find("Player").GetComponent<MouseLook>().enabled = false; //F
-
+        readyB.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
-
-    public void Resume()
-    {
-        paused = false;
-        GameObject.Find("Main Camera").GetComponent<WowCamera>().enabled = true; //F
-        GameObject.Find("Player").GetComponent<MouseLook>().enabled = true; //F
     }
 
     public void MainMenu()
@@ -56,6 +50,7 @@ public class SelectHero : MonoBehaviour {
 
     public void HeroPickRight()
     {
+        readyB.enabled = true;
         selectR = true;
         selectL = false;
         light_warrior.SetActive(true);
@@ -65,6 +60,7 @@ public class SelectHero : MonoBehaviour {
     }
     public void HeroPickLeft()
     {
+        readyB.enabled = true;
         selectR = false;
         selectL = true;
         light_mage.SetActive(true);
@@ -73,24 +69,29 @@ public class SelectHero : MonoBehaviour {
 
     public void Ready()
     {
-        isSelecting = false;
-        gui.SetActive(true);
-        GameObject.Find("PickHero").SetActive(false);
-        if (selectR)
+        if (selectL || selectR)
         {
-            mage.SetActive(true);
-            fire.SetActive(true);
-        }
-        if (selectL)
-        {
-            overlord.SetActive(true);
-        }
-        paused = !paused;
-        GameObject.Find("Main Camera").GetComponent<WowCamera>().enabled = true; //F
-        GameObject.Find("Player").GetComponent<MouseLook>().enabled = true; //F
+            isSelecting = false;
+            gui.SetActive(true);
+            cam.GetComponent<WowCamera>().enabled = true;
 
-        Time.timeScale = 1;
-        Cursor.lockState = CursorLockMode.Locked; //Le curseur disparait et est lock au milieu de l'ecran
-        Cursor.visible = false;
+            if (selectL)
+            {
+                mage.SetActive(true);
+                fire.SetActive(true);
+                cam.GetComponent<WowCamera>().target = mage.transform;
+            }
+            if (selectR)
+            {
+                overlord.SetActive(true);
+                cam.GetComponent<WowCamera>().target = overlord.transform;
+            }
+            paused = !paused;
+            GameObject.Find("Player").GetComponent<MouseLook>().enabled = true; //F
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked; //Le curseur disparait et est lock au milieu de l'ecran
+            Cursor.visible = false;
+            GameObject.Find("PickHero").SetActive(false);
+        }     
     }
 }
