465a7f2f527c95fd8df280ae01c7fa4dd8ed65dc
sarrid <ridaysarkar60@gmail.com>
2016-05-09 23:23:08 +0200

    reseau : from the scratch

---
+++ Forteresse/Assets/PlayerSetUp.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetUp : NetworkBehaviour
+{
+    Camera SceneCamera;
+    [SerializeField]
+    Behaviour[] componentToDisable;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentToDisable.Length; i++)
+            {
+                componentToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            SceneCamera = Camera.main;
+            if (SceneCamera != null)
+            {
+                SceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnDisable()
+    {
+        if (SceneCamera != null)
+        {
+            SceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
