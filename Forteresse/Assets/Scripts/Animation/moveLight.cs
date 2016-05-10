d96f62f6a75dbbd0445f4e8fd05f78b0d2906471
Slourf <florian.chatelus@yahoo.fr>
2016-05-11 00:05:57 +0200

    ajout du Tower Buffer : tout debut

---
+++ Forteresse/Assets/Scripts/Animation/moveLight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveLight : MonoBehaviour {
+
+    float time = 0;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (time > 100)
+            time = 0f;
+        time += 1 * Time.deltaTime;
+        GetComponent<Light>().intensity = 3 * Mathf.Sin(3f * time) + 5;
+    }
+}
