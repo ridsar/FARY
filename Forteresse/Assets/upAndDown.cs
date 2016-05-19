7fb0804b5327251ea6d92face8b1f4f2043a9044
Slourf <florian.chatelus@yahoo.fr>
2016-05-19 22:04:53 +0200

    New Tower : Stun Trap -> début d'annimation;

---
+++ Forteresse/Assets/upAndDown.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class upAndDown : MonoBehaviour {
+
+    bool check;
+    Transform child;
+
+	// Use this for initialization
+	void Start ()
+    {
+        child = transform.GetChild(0).transform;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (!check)
+        {
+            child.localPosition = Vector3.Lerp(child.localPosition, new Vector3(0, 0.3f, 0), 0.1f);
+        }
+	}
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            check = true;
+            child.localPosition = Vector3.Lerp(child.localPosition, new Vector3(0, 0.8f, 0), 0.2f);
+        }
+        else
+        {
+            check = check || false;
+        }   
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            check = false;
+        }
+    }
+}
