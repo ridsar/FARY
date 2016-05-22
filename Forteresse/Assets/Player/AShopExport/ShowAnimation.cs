22c47690f2f3e6a5fc40da615b2a85ba0f748c25
henric_a <arnaud.henric@epita.fr>
2016-05-23 00:55:48 +0200

    mage animé

--- Forteresse/Assets/Player/AShopExport/ShowAnimation.cs
+++ Forteresse/Assets/Player/AShopExport/ShowAnimation.cs
@@ -4,22 +4,52 @@ using UnityEditor;
 
 public class ShowAnimation : MonoBehaviour {
 
-	public GameObject [] AinObjs;
-	private int CurAinObjCount =0;
-	//public GameObject AinObj;
-	private Animation ain;
-	public AnimationClip [] clips;
-	public int CurAnimClip =0;
-	public string CurAnimName;
-	// Use this for initialization
-	void Start () {
-		AddAnim ();
+    public GameObject[] AinObjs;
+    private int CurAinObjCount = 0;
+    //public GameObject AinObj;
+    private Animation ain;
+    public AnimationClip[] clips = new AnimationClip[5];
+    public int CurAnimClip = 1;
+    public string CurAnimName;
+    public float inputH;
+    public float inputV;
+    // Use this for initialization
+    void Start () {
+		//AddAnim ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        if (Input.GetMouseButton(0))
+        {
+            CurAnimClip = 2;
+            PlayAnim();
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            CurAnimClip = 2;
+            PlayAnim();
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            CurAnimClip = 3;
+            PlayAnim();
+        }
+        else if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.S))
+        {
+            CurAnimClip = 0;
+            PlayAnim();
+        }
+        else
+        {
+            CurAnimClip = 1;
+            PlayAnim();
+        }
+
+    }
 	
 	 void OnGUI(){
 		if (GUI.Button(new Rect(10, 210, 90, 50), "NestAnim"))
@@ -60,15 +90,15 @@ public class ShowAnimation : MonoBehaviour {
 			i =0;
 		}
 		AinObjs [i].SetActive(true);
-		AddAnim ();
+		//AddAnim ();
 	}
 
 	
-	void AddAnim () 
+	/*void AddAnim () 
 	{
 		ain = AinObjs [i].GetComponent<Animation>();
 		clips = AnimationUtility.GetAnimationClips(ain);
-	}
+	}*/
 	void PlayAnim ()
 	{
 		AinObjs [i].GetComponent<Animation>().Play (clips [CurAnimClip].name);
---
+++ Forteresse/Assets/Scripts/Animation/AnimationsMage.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationsMage : MonoBehaviour {
+
+    public Animator anim;
+    public Animation animations;
+    public float inputH;
+    public float inputV;
+    public bool jump;
+    public bool run;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        anim.SetFloat("inputH", inputH);
+        anim.SetFloat("inputV", inputV);
+        if (Input.GetMouseButton(0))
+        {
+            anim.SetBool("attack", true);
+            animations.Play();
+        }
+        else
+        {
+            anim.SetBool("attack", false);
+        }
+    }
+}
