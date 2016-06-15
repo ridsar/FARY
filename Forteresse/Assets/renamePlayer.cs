d274945e68f17f6dbc01be26704bfa9603b2a596
Slourf <florian.chatelus@yahoo.fr>
2016-06-15 13:02:26 +0200

    Resolustion du pb de Riday sur le reseau ca fait environ une ligne le mec est vraimentmauvais quoi:O

---
+++ Forteresse/Assets/renamePlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class renamePlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameObject.name = "Player";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
